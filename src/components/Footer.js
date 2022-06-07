44c572718c031f67fa016dd5dad664c013f6d896
leekyukin <leekujin14@gmail.com>
2022-06-07 11:57:43 +0900

    ADD :: footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+                <span className={"logo"}>@2018 PEACEMINUSONE</span>
+                <span className={"box"}>
+                    <a href={"#"} className={"menu-el"}> TEAMS OF USE</a>
+                    <a href={"#"} className={"menu-el"}> PRIVACY POLICY</a>
+                    <a href={"#"} className={"menu-el"}> HELP</a>
+                </span>
+            </div>
+            <div></div>
+            <div>
+                <a href={"http:/www.instagram.com/x_xxi_lkj"}>INSTAGRAM</a>
+            </div>
+        </footer>
+    );
+}
