52ba91fb1e551907bc0125ec0f95f0fe1024cd58
leekyukin <leekujin14@gmail.com>
2022-06-07 11:57:21 +0900

    ADD :: main page

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import bg from "../img/background.png"
+
+export default function Main() {
+    return (
+      <div className={"main"}>
+       <img className={"background"} src={bg}/>
+      </div>
+    );
+}
